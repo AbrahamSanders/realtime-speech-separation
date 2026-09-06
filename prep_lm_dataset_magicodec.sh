04ade4cc7120df510818119f790145ba69170b93
AbrahamSanders <abraham.sanders@gmail.com>
2026-09-06 16:16:26 -0400

    Add dataset split script

--- prep_lm_dataset_magicodec.sh
+++ prep_lm_dataset_magicodec.sh
@@ -1,3 +1,6 @@
 python prep_lm_dataset.py \
     --codes_path=data/audio/codes/MagiCodec-50Hz-Base/0.1s_2.0s \
     --save_path=output/dataset_magicodec_131k_40s.txt
+
+python tools/split_lm_dataset.py \
+    --dataset_path=output/dataset_magicodec_131k_40s.txt
