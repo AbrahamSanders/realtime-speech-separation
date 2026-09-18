1d77cfa944507db4303cf0bb6e09d0a2a76ff1e9
AbrahamSanders <abraham.sanders@gmail.com>
2026-09-18 16:44:55 -0400

    find short enrollments tool

---
+++ tools/find_short_enrollments.py
@@ -0,0 +1,10 @@
+import json
+
+with open("../output/dataset_magicodec_131k_60s_metadata.jsonl", "r") as f:
+    metadata = [json.loads(line) for line in f]
+
+short_enrollments = [m for m in metadata if m["tv_end_secs"] - m["tv_start_secs"] < 3.0]
+
+print(f"Found {len(short_enrollments)} short enrollments.")
+for m in short_enrollments:
+    print(m)
